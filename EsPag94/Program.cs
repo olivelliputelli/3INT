ab41bc00e7b319b92773de2f963ffd02d551df5d
Olivelli Putelli <olivelliputelli@live.it>
2017-09-14 12:07:17 +0200

    Creata Soluzione

--- EsPag94/Program.cs
+++ EsPag94/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EsPag94
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int anno, mese, giorno, Y, M, D;
+
+        // INPUT
+        // cout << "Anno: ";
+        // cin >> anno;
+        Console.Write("Anno: ");
+        anno = int.Parse(Console.ReadLine());
+
+        Console.Write("Mese: ");
+        mese = int.Parse(Console.ReadLine());
+
+        Console.Write("Giono: ");
+        giorno = int.Parse(Console.ReadLine());
+
+        //ELABORAZIONE
+        Y = anno;
+        M = mese - 2;
+
+        if(M <= 0)
         {
+            Y = Y - 1;
+            M = M + 12;
         }
+        D = ((giorno + Y + (Y / 4) - (Y / 100) + (Y / 400) + (31 * M / 12)) % 7);
+
+        //OUTPUT
+        Console.WriteLine($"Risultato = {D}");      
+
+
     }
 }
+
